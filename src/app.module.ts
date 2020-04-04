2490f27ecf977284e904c8e06d2db7048546a419
Nhan Cao <nhancv92@gmail.com>
2020-04-04 10:05:47 +0700

    add env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from "@nestjs/serve-static";
@@ -6,6 +7,7 @@ import { join } from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       exclude: ['/api*'],
