f58dbec8c3eb51e61367561d49c38c116032c9a9
Nhan Cao <nhancv92@gmail.com>
2020-05-18 10:57:43 +0700

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import {NestFactory} from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  
+  // Enable Swagger api docs module
+  const options = new DocumentBuilder()
+  .setTitle('Default example')
+  .setDescription('The default API description')
+  .setVersion('1.0')
+  .addTag('default')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
