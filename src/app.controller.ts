4ff6c3c0e8f54cc1f919e3357f5451bc13bf657b
Nhan Cao <nhancv92@gmail.com>
2020-08-04 21:03:49 +0700

    feat: update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import {Controller, Get} from '@nestjs/common';
 import {AppService} from './app.service';
 import {BaseResponse} from "./responses/base.response";
-import {ApiTags} from "@nestjs/swagger";
+import {ApiOkResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
 
 @ApiTags('app')
 @Controller()
@@ -10,6 +10,11 @@ export class AppController {
   }
   
   @Get()
+  @ApiOkResponse({
+    description: 'Test app works',
+    type: String,
+  })
+  @ApiOperation({summary: 'hello'})
   getHello(): BaseResponse<string> {
     const response: BaseResponse<string> = {}
     response.data = this.appService.getHello();
