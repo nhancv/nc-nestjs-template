686718f7a0fe1bb81fa12e18391a488212f01d46
Nhan Cao <nhancv92@gmail.com>
2021-03-10 11:04:47 +0700

    feat: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
   },
 };
