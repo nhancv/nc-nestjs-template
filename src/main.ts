f51f6294ad65602e9ba0fac1e971970a21e2ba5d
Nhan Cao <nhancv92@gmail.com>
2021-03-10 22:49:16 +0700

    feat: update standalone applications

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import {NestFactory} from '@nestjs/core';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {AppService} from "./app.service";
 import {AppModule} from './app.module';
 import {AllExceptionsFilter} from "./utils/all-exceptions.filter";
 import {Logger, ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
+  //------Standalone Applications------//
+  // https://docs.nestjs.com/standalone-applications
+  // Application logic......
+  //
+  // const appService = app.get(AppService);
+  // logger.log(appService.getHello());
+
+  //------Web Applications------//
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
-
   // Enable Swagger api docs module
   const options = new DocumentBuilder()
     .setTitle('Default example')
