e2c51532c8a7b09d455f5c3cbea400c52d80bdf4
Nhan Cao <nhancv92@gmail.com>
2021-12-24 22:49:29 +0700

    feat: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,21 +14,23 @@
 // # Delete all
 // pm2 delete ecosystem.config.js
 module.exports = {
-  apps: [{
-    name: "nestjs-prod",
-    script: "./dist/main.js", // cluster mode run with node only, not npm
-    args: "",
-    exec_mode : "cluster", // default fork
-    instances: 2, //"max",
-    kill_timeout : 4000,
-    wait_ready: false,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: "1G",
-    log_date_format : "YYYY-MM-DD HH:mm Z",
-    env: {
-      ENABLE_WEB: 'true',
-      ENABLE_WORKER: 'false',
+  apps: [
+    {
+      name: 'nestjs-prod',
+      script: './dist/main.js', // cluster mode run with node only, not npm
+      args: '',
+      exec_mode: 'cluster', // default fork
+      instances: 2, //"max",
+      kill_timeout: 4000,
+      wait_ready: false,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      env: {
+        ENABLE_WEB: 'true',
+        ENABLE_WORKER: 'false',
+      },
     },
-  }]
+  ],
 };
