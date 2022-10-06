ad56a5feb73907e10be4c0b5bfd9d40a11040414
Nhan Cao <nhancv92@gmail.com>
2022-10-06 08:44:24 +0700

    feat: update cron example

--- src/packages/cron/cron.service.ts
+++ src/packages/cron/cron.service.ts
@@ -1,18 +1,24 @@
-import {Injectable, Logger} from '@nestjs/common';
-import {CronExpression, SchedulerRegistry} from '@nestjs/schedule';
-import {CronJob} from "cron";
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
-  constructor(private readonly schedulerRegistry: SchedulerRegistry) {
+  constructor(private readonly schedulerRegistry: SchedulerRegistry) {}
+
+  @Cron(CronExpression.EVERY_30_SECONDS, {
+    name: 'notifications',
+  })
+  handleAutoCron() {
+    this.logger.debug('Called every 30 seconds');
   }
 
   // Create dynamically new cron job
   private addCronJob(name: string, cronTime: string, logic: () => any): void {
     const job = new CronJob(cronTime, async () => {
-      if(logic) {
+      if (logic) {
         await logic();
       }
     });
@@ -20,19 +26,21 @@ export class CronService {
     this.schedulerRegistry.addCronJob(name, job);
     job.start();
 
-    this.logger.warn(
-      `[${cronTime}]job ${name} added!`,
-    );
+    this.logger.warn(`[${cronTime}]job ${name} added!`);
   }
+
+  // Test logic
+  _business = async (): Promise<void> => {
+    console.log('Cron job');
+  };
 
   // Start cron job
   startCronJobs(): void {
     // CronExpression.EVERY_5_SECONDS
-    this.addCronJob('test_cronjob', CronExpression.EVERY_5_SECONDS, CronService.testOutput);
-  }
-
-  // Test logic
-  private static async testOutput(): Promise<void> {
-    console.log('Cron job');
+    this.addCronJob(
+      'test_cronjob',
+      CronExpression.EVERY_5_SECONDS,
+      this._business,
+    );
   }
 }
