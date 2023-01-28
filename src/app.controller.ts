7e2f9d643a5e10220d6d43d625d7956ce2dd8d3a
Nhan Cao <nhancv92@gmail.com>
2023-01-28 11:03:10 +0700

    feat: update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { BaseResponse } from './utils/base.response';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
 
 @ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Throttle(3, 60)
   @Get()
   @ApiOkResponse({
     description: 'Test app works',
